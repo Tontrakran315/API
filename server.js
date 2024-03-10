678ba0c651c819f6f839b9362b15c3002195dbe0
Tontrakran <kimza617@gmail.com>
2024-03-10 21:59:06 +0700

    server.js

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const app = express()
+const books = require('./db')
+app.get('/', (req, res) => {
+  res.send('Hello World')
+})
+
+app.get('/books', (req, res) => {
+    res.json(books)
+  })
+
+ app.get('/books/:id', (req, res) => {
+    res.json(books.find(book => book.id === req.params.id))
+  })
+app.post('/books', (req, res) => {
+    books.push(req.body)
+    res.status(200).json(req.body)
+  })
+
+const bodyParser = require('body-parser')
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.put('/books/:id', (req, res) => {
+    const updateIndex = books.findIndex(book => book.id === req.params.id)
+    res.json(Object.assign(books[updateIndex], req.body))
+  })
+
+app.delete('/books/:id', (req, res) => {
+    const deletedIndex = books.findIndex(book => book.id === req.params.id)
+    books.splice(deletedIndex, 1)
+    res.status(200).send()
+ })
+
+app.listen(3000, () => {
+  console.log('http://localhost:3000')
+})
